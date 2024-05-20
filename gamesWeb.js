4f4aa46fa1d5cde7199c3571c77e90007d8811b9
PedroHenriqueMandajiStipp <161366225+PedroHenriqueMandajiStipp@users.noreply.github.com>
2024-05-20 18:51:12 +0000

    la

--- gamesWeb.js
+++ gamesWeb.js
@@ -1,23 +1,43 @@
-function movimentaNaveteclado(tecla) {
-  ctx.clearRect(x,700-160, 160, 160);
-  if (tecla == 37) { // Seta para esquerda
-    x -= 50;
-    x = Math.max(0, x);
-  } else if (tecla == 39) { // Seta para direita
-    x += 50;
-    x = Math.min(canvas.width -160, X);
-  } else if (tecla == 32) {// espaço
-    disparaTiro(x+80, 700-160);
+function colidiu() {
+  var largura = nave.width;
+  var altura = nave.height;
+  return (xNave + largura) > xInimigo &&
+  xNave < (xInimigo + largura) &&
+  (yNave + altura) > yInimigo &&
+  yNave < (yInimigo + altura)
   }
-  ctx.drawimage(nave, x, 700-160);
-}
-
-function disparaTiro(col, lin) {
-  if (lin > -6) {
-    ctx.clearRect(col, lin, 2, 5);
-    ctx.fillRect(col, lin-6, 2, 5);
+  function movimentaNaveInimigo(tecla) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  if (tecla == "ArrowLeft") { // Seta para esquerda
+  xNave -= 50;
+  xNave = Math.max(0, xNave);
+  } else if (tecla == "ArrowRight") { // Seta para direita
+  xNave += 50;
+  xNave = Math.min(canvas.width - 160, xNave);
+  } else if (tecla == "ArrowUp") { // Seta para cima
+  yNave -= 50;
+  yNave = Math.max(0, yNave);
+  } else if (tecla == "ArrowDown") { // Seta para baixo
+  yNave += 50;
+  yNave = Math.min(canvas.height - nave.height, yNave);
+  } else if (tecla == "a") { // Tecla A
+  xInimigo -= 50;
+  xInimigo = Math.max(0, xInimigo);
+  } else if (tecla == "d") { // Tecla D
+  xInimigo += 50;
+  xInimigo = Math.min(canvas.width - 160, xInimigo);
+  } else if (tecla == "w") { // Tecla W
+  yInimigo -= 50;
+  yInimigo = Math.max(0, yInimigo);
+  } else if (tecla == "s") { // Tecla S
+  yInimigo += 50;
+  yInimigo = Math.min(canvas.height - inimigo.height, yInimigo);
   }
-  setTimeout(function(){ disparaTiro(col, lin-6) }, 1);
-}
-    
-    
+  ctx.drawImage(nave, xNave, yNave);
+  ctx.drawImage(inimigo, xInimigo, yInimigo);
+  
+  if (colidiu()) {
+  alert("Chocou uma com a outra!");
+  }
+  }
